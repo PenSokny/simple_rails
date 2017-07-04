4ce610df0a8707d43176f758ffd6bd1e28097c20
PenSokny <pen.sokhny@gmail.com>
2017-07-04 15:50:10 +0700

    Revert "Basic login lesson"
    This reverts commit 7f7a46bdd0d92a8dee668c9206a00bdce2cd0d80.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: Settings.password.minimum}
-  validates :password_confirmation, presence: true,
-    length: {minimum: Settings.password.minimum}, on: [:create]
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.password.minimum}
+
   has_secure_password
 
   before_save :email_downcase
-
+  
   private
   def email_downcase
     email.downcase!
