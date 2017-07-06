1294ace25a00c316d34921be92ba1d14efe140a1
PenSokny <pen.sokhny@gmail.com>
2017-07-06 13:41:13 +0700

    Advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,47 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: Settings.password.minimum}
-  validates :password_confirmation, presence: true,
-    length: {minimum: Settings.password.minimum}, on: [:create]
+  validates :password, presence: true, allow_nil: true,
+  length: {minimum: Settings.password.minimum}
   has_secure_password
 
   before_save :email_downcase
 
+  class << self
+    def digest string
+      if ActiveModel::SecurePassword.min_cost
+        cost = BCrypt::Engine::MIN_COST
+      else
+        cost = BCrypt::Engine.cost
+      end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest.present?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
   private
+
   def email_downcase
-    email.downcase!
+    self.email = email.downcase
   end
 end
